0a855fea4868ab5e20d37eb7420a95ce808d5e18
Safdar-Siddiqui <safdarsiddiqui25@gmail.com>
2023-09-30 18:42:17 +0500

    Assignment04

---
+++ app.js
@@ -0,0 +1,15 @@
+let heading1 = document.getElementById("heading1");
+let heading2 = document.getElementById("heading2");
+let var1, var2, var3, var4;
+
+var1="Muhammad "; 
+var2="Safdar ";
+var3="Siddiqui";
+var4="CCO-9089598";
+
+heading1.innerHTML= var1 + var2 + var3;
+heading2.innerHTML= var4;
+
+// --------------- task2---------------
+
+
